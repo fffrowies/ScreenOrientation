382900624d37ac8b35f08c3b2c87c55ac6411e05
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-06 11:58:21 -0300

    Restoring Activity State

---
+++ app/src/main/java/com/fffrowies/screenorientation/Constants.java
@@ -0,0 +1,6 @@
+package com.fffrowies.screenorientation;
+
+public class Constants {
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_BTN_TXT = "btn_txt";
+}
--- app/src/main/java/com/fffrowies/screenorientation/MainActivity.java
+++ app/src/main/java/com/fffrowies/screenorientation/MainActivity.java
@@ -2,12 +2,101 @@ package com.fffrowies.screenorientation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView txvLoginMessage;
+    private EditText edtName;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate()");
+
+        txvLoginMessage = findViewById(R.id.txvLoginMessage);
+        edtName = findViewById(R.id.edtName);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txvLoginMessage.setText("Welcome " + edtName.getText().toString());
+                btnLogin.setText("Logout");
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(TAG, "onRestoreInstanceState()");
+
+        if (savedInstanceState != null) {
+            btnLogin.setText(savedInstanceState.getString(Constants.KEY_BTN_TXT));
+            txvLoginMessage.setText(savedInstanceState.getString(Constants.KEY_MESSAGE));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(TAG, "onSaveInstanceState()");
+
+        outState.putString(Constants.KEY_BTN_TXT, btnLogin.getText().toString());
+        outState.putString(Constants.KEY_MESSAGE, txvLoginMessage.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy()");
     }
 }
